53079f0f3f224a0b4bdf811a078db73801b3d61b
ferradasdiego <ies.a15diegocf@gmail.com>
2022-07-02 09:59:53 +0200

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
-const App = ()=>{
-  return null;
-}
+import { useState } from 'react';
 
-export default App
+const getInitialLikes = () => {
+  console.log('getInitialLikes');
+  return 12;
+};
+
+const Likes = () => {
+  console.log('Render Likes');
+
+  const [likes, setLikes] = useState(getInitialLikes);
+
+  return <Button onClick={() => setLikes(likes + 1)}>{likes} likes</Button>;
+};
+
+const Button = ({ onClick, children }) => {
+  console.log('Render Button');
+  return <button onClick={onClick}>{children}</button>;
+};
+
+const App = () => {
+  console.log('Render App');
+  return <Likes />;
+};
+
+export default App;
